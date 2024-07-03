fc2789d6cb85dd1fa71978d1cd705fe9d51ed3db
maria <166440596+eduardajp@users.noreply.github.com>
2024-07-03 16:50:11 -0300

    Add files via upload

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { HomeComponent } from './pages/home/home.component';
+import { CadUsuarioComponent } from './pages/usuario/cad-usuario/cad-usuario.component';
+
+const routes: Routes = [
+  {path:'',redirectTo:'login',pathMatch:'full'},
+  {path:'login',component:LoginComponent},
+  {path:'home',component:HomeComponent},
+  {path:'cadUsuario',component:CadUsuarioComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
